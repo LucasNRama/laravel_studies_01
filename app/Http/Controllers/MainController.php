e311367f73b3d020d3658d40dddbb2759aa3d958
LucasNRama <lucasrama2002@gmail.com>
2024-11-06 14:30:10 -0300

    004

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -3,31 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\View\View;
 
 class MainController extends Controller
 {
-    public function index(){
-        echo 'index';
+    public function initMethod() : string
+    {
+        return 'hello world';
+        // echo 'hello world';
     }
-    public function about(){
-        echo 'about';
-    }
-    public function mostrarValor($valor){
-        echo "Valor enviado pela rota: $valor";
-    }
-    public function mostrarValores($valor1, $valor2){
-        echo "Valores enviados pela rota: $valor1 e $valor2";
-    }
-    public function mostrarValores2(Request $request, $valor1, $valor2){
-        echo "Valores enviados pela rota: $valor1 e $valor2";
-    }
-    public function mostrarValorOpcional($valor = null){
-        echo "Valor opcional: $valor";
-    }
-    public function mostrarValorOpcional2($valor1, $valor2 = 100){
-        echo "Valor opcional: $valor1 e $valor2";
-    }
-    public function mostrarPosts($user_id, $post_id){
-        echo "Posts do usuário ID: $user_id e o post com ID: $post_id";
+
+    public function viewPage(): View
+    {
+        return view('home');
     }
 }
