2814b62dfd537069ec2e93d0995002e8e3a88422
LucasNRama <lucasrama2002@gmail.com>
2024-11-06 15:56:36 -0300

    008

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,10 +4,5 @@ namespace App\Http\Controllers;
 
 abstract class Controller
 {
-    public function cleanUpperCaseString($string) : string
-    {
-        // remover os espaços em branco no início e fim de uma string
-        // converter a string para uppercase(maiúsculos)
-        return strtoupper(trim($string));
-    }
+    //
 }
